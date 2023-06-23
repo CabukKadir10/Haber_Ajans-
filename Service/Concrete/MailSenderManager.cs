2671dad8404efb0d0a45ca7159cf3e0072502005
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-06-23 22:19:58 +0300

    Update MailSenderManager.cs

--- Service/Concrete/MailSenderManager.cs
+++ Service/Concrete/MailSenderManager.cs
@@ -13,9 +13,9 @@ namespace Service.Concrete
     {
         public void SendMail(string body)
         {
-            var fromAddress = new MailAddress("cabuk0539@zohomail.eu");
-            var toAddress = new MailAddress("cabuk0539@gmail.com");
-            const string subject = "Haber Ajansı | Kadir Çabuk Deneme Maili";
+            var fromAddress = new MailAddress("GönderenMail@zohomail.eu");
+            var toAddress = new MailAddress("AlıcıMail@gmail.com");
+            const string subject = "Haber Ajansı | Reshoo Deneme Maili";
             using (var smtp = new SmtpClient
             {
                 Host = "smtp.zoho.eu",
@@ -23,7 +23,7 @@ namespace Service.Concrete
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, "U8YdwLAFufvGrk!")
+                Credentials = new NetworkCredential(fromAddress.Address, "Password")
             })
             {
                 using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
